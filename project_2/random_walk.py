84f5cf03169408eb019058cea125696befa5d34a
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-07-28 15:48:01 -0400

    Update random_walk.py

--- project_2/random_walk.py
+++ project_2/random_walk.py
@@ -11,21 +11,24 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """
+        GETS THE DIRECTION FOR EACH NEW POINT IN THE RANDOM WALK
+        """
+        # DECIDE WHICH DIRECTION TO GO AND HOW FAR TO GO IN THAT DIRECTION
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        step = direction * distance
+
+        return step
+
     def fill_walk(self):
         """CALCULATE ALL THE POINTS IN THE WALK."""
 
         # KEEP TAKING STEPS UNTIL THE WALK REACHES THE DESIRED LENGTH
         while len(self.x_values) < self.num_points:
-
-            # DECIDE WHICH DIRECTION TO GO AND HOW FAR TO GO IN THAT DIRECTION
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
+            x_step = self.get_step()
+            y_step = self.get_step()
             # REJECT MOVES THAT GO NOWHERE
             if x_step == 0 and y_step == 0:
                 continue

7579d9f14965aa059db3a3543fe2fb778a8a1d6c
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-07-29 12:00:51 -0400

    Add files via upload

---
+++ project_2/death_valley_highs_lows.py
@@ -0,0 +1,40 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # GET DATES AND HIGH AND LOW TEMPERATURES FROM THIS FILE
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# PLOT THE HIGH TEMPERATURES
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# FORMAT PLOT
+title = "Daily high and temperatures - 2018\n Death Valley"
+ax.set_title(title, fontsize=20)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()

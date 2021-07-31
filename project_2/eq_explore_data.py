1d651934485ff070eae3e88065d3dea943a6da91
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-07-31 15:07:32 -0400

    Add files via upload

---
+++ project_2/eq_explore_data.py
@@ -0,0 +1,26 @@
+import json
+
+# EXPLORE THE STRUCTURE OF THE DATA
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])

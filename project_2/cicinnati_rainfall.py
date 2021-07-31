b1e78abbfb305cdff2b455b5258b40b81c7c167c
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-07-31 11:00:59 -0400

    Add files via upload

---
+++ project_2/cicinnati_rainfall.py
@@ -0,0 +1,37 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'data/cincinnati_weather_2020.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+
+    # GET THE DATES AND PRECIPITATION LEVELS
+    dates, rainfalls = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%m/%d/%Y')
+        try:
+            rainfall = float(row[3])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            rainfalls.append(rainfall)
+
+# PLOT THE HIGH TEMPERATURES
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, rainfalls, c='red')
+
+# FORMAT PLOT
+title = "Daily rainfall - 2020\n Cincinnati"
+ax.set_title(title, fontsize=20)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Rainfall (in)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()

f06ddb8829bf31fff514acfe0b0dc483e014484c
Theotecles <45003847+Theotecles@users.noreply.github.com>
2021-08-01 11:26:18 -0400

    Add files via upload

---
+++ project_2/python_repos.py
@@ -0,0 +1,26 @@
+import requests
+
+# MAKE AN API CALL AND STORE THE RESPONSE
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# STORE API RESPOSNSE IN A VARIABLE
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+# EXPLORE INFORMATION ABOUT THE REPOSITORIES
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# EXAMINE THE FIRST REPOSITORY
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description {repo_dict['description']}")
